29a8e3ece930c98d8247926e72627b02c465014d
Anastasiia Nykytiuk-Nesenchuk <siia.n.n.996@gmail.com>
2023-11-18 18:11:37 +0200

    Learned void method

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,11 +1,29 @@
-﻿int x = 3;
-int y = 2;
+﻿void getStageByAge(int age)
+{
+    if (age <= 0)
+    {
+        Console.WriteLine("Incorrect age!");
+        return;
+    }
+    if (age > 18)
+    {
+        Console.WriteLine("You are adult");
+        return;
+    }
+    if (age >= 14)
+    {
+        Console.WriteLine("You are teenager");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("You are child");
+        return;
+    }
+}
 
-int z = x > y ? x : y;
 
-/*
-if (x > y)
-    z = x;
-else
-    z = y;
-*/
+getStageByAge(15);
+getStageByAge(23);
+getStageByAge(-2);
+getStageByAge(3);
