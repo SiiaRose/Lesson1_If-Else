8a68601d8b694fbdcb9a27af692b6f11857b1c76
Anastasiia Nykytiuk-Nesenchuk <siia.n.n.996@gmail.com>
2023-11-18 14:54:25 +0200

    Learned operator if-else

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,11 +1,18 @@
-﻿float weight = 59f;
-int age = 26;
+﻿int age = 26;
 
-if (age >= 18 && weight >= 50 && age < 50)
+if (age > 18)
 {
-    Console.WriteLine("You are allowed to donor your blood");
+    Console.WriteLine("You are adult");
+}
+if(age >= 14)
+{
+    Console.WriteLine("You are teenager");
+}
+if (age <= 0)
+{
+    Console.WriteLine("Invalid age");
 }
 else
 {
-    Console.WriteLine("You aren't allowed to donor your blood");
+    Console.WriteLine("You are child");
 }
