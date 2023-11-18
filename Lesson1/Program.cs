8eca5a0ae30331073ad282150bbdcc85951e25bc
Anastasiia Nykytiuk-Nesenchuk <siia.n.n.996@gmail.com>
2023-11-18 14:40:52 +0200

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,11 @@
+﻿float weight = 59f;
+int age = 26;
+
+if (age >= 18 && weight >= 50 && age < 50)
+{
+    Console.WriteLine("You are allowed to donor your blood");
+}
+else
+{
+    Console.WriteLine("You aren't allowed to donor your blood");
+}
